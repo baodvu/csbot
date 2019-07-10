8cc687eef49e42540bb5e483e24c8ed499243e0f
Bao Vu <baodvu@gmail.com>
2019-07-09 20:16:40 -0700

    Switch to unirest for cleaner code

--- src/main/java/com/codesignal/csbot/watchers/MainWatcher.java
+++ src/main/java/com/codesignal/csbot/watchers/MainWatcher.java
@@ -1,16 +1,26 @@
 package com.codesignal.csbot.watchers;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainWatcher {
+    private static final Logger logger = LoggerFactory.getLogger(MainWatcher.class);
+
     public static void init() {
         final int DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
 
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                new StockMarketCalendarWatcher().run();
+                try {
+                    new StockMarketCalendarWatcher().run();
+                } catch (UnirestException exp) {
+                    logger.error("Failed to get data from investing.com");
+                }
             }
         }, 0L, DAY_IN_MILLISECONDS);
     }
--- src/main/java/com/codesignal/csbot/watchers/StockMarketCalendarWatcher.java
+++ src/main/java/com/codesignal/csbot/watchers/StockMarketCalendarWatcher.java
@@ -1,26 +1,21 @@
 package com.codesignal.csbot.watchers;
 
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.util.Map;
 
 class StockMarketCalendarWatcher {
     private static final Logger logger = LoggerFactory.getLogger(StockMarketCalendarWatcher.class);
 
-    private static HttpClient client = HttpClient.newHttpClient();
+    void run() throws UnirestException {
+        this.getTomorrowEvents();
+    }
 
-    private void getTomorrowEvents() {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.investing.com/economic-calendar/Service/getCalendarFilteredData"))
-                .timeout(Duration.ofMinutes(2))
+    private void getTomorrowEvents() throws UnirestException {
+        String data = Unirest
+                .post("https://www.investing.com/economic-calendar/Service/getCalendarFilteredData")
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .header("X-Requested-With", "XMLHttpRequest")
                 .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, " +
@@ -28,48 +23,52 @@ class StockMarketCalendarWatcher {
                 .header("Origin", "https://www.investing.com")
                 .header("Referer", "https://www.investing.com/economic-calendar/?utm_source=email&utm_medium=email" +
                         "&utm_campaign=CTA")
-                .POST(HttpRequest.BodyPublishers.ofString("country%5B%5D=25&country%5B%5D=32&country%5B%5D=6&country" +
-                        "%5B%5D=37&country%5B%5D=72&country%5B%5D=22&country%5B%5D=17&country%5B%5D=39&country%5B%5D" +
-                        "=14&country%5B%5D=10&country%5B%5D=35&country%5B%5D=43&country%5B%5D=56&country%5B%5D=36" +
-                        "&country%5B%5D=110&country%5B%5D=11&country%5B%5D=26&country%5B%5D=12&country%5B%5D=4" +
-                        "&country%5B%5D=5&importance%5B%5D=3&timeZone=8&timeFilter=timeRemain&currentTab=tomorrow" +
-                        "&limit_from=0"))
-                .build();
+                .field("country[]", "25")
+                .field("country[]", "32")
+                .field("country[]", "6")
+                .field("country[]", "37")
+                .field("country[]", "72")
+                .field("country[]", "22")
+                .field("country[]", "17")
+                .field("country[]", "39")
+                .field("country[]", "14")
+                .field("country[]", "10")
+                .field("country[]", "35")
+                .field("country[]", "43")
+                .field("country[]", "56")
+                .field("country[]", "36")
+                .field("country[]", "110")
+                .field("country[]", "11")
+                .field("country[]", "26")
+                .field("country[]", "12")
+                .field("country[]", "4")
+                .field("country[]", "5")
+                .field("importance[]", "2")
+                .field("timeZone", "8")
+                .field("timeFilter", "timeRemain")
+                .field("currentTab", "tomorrow")
+                .field("limit_from", "0")
+                .asJson().getBody().getObject().get("data").toString();
 
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(StockMarketCalendarWatcher::formatTable);
+        this.process(data);
     }
 
-    void run() {
-        this.getTomorrowEvents();
-    }
-
-    private static void formatTable(String htmlData) {
-        JSONObject jsonObj = new JSONObject(new JSONTokener(htmlData));
-
-        String data = (String) jsonObj.get("data");
-
+    private void process(String data) throws UnirestException{
         String marketCalendarNotice = data
                 .replaceAll("</tr>", "\n")
                 .replaceAll("<[^>]+>", "")
                 .replaceAll("&nbsp;", " ")
                 .replaceAll(" +", " ");
 
-        Map<String, String> message = Map.of(
-                "content", "https://www.investing.com/economic-calendar\n```" + marketCalendarNotice.strip() + "```"
-        );
+        String content = "https://www.investing.com/economic-calendar\n```" + marketCalendarNotice.strip() + "```";
 
         String YACHT_BOT_WEBHOOK = System.getenv("YACHT_BOT_WEBHOOK");
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(YACHT_BOT_WEBHOOK))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(new JSONObject(message).toString()))
-                .build();
+        String response = Unirest.post(YACHT_BOT_WEBHOOK)
+                .header("Accept", "application/json")
+                .field("content", content)
+                .asString().getBody();
 
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(logger::info);
+        logger.info(response);
     }
 }
