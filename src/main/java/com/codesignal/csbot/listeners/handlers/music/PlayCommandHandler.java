e85ee9c4194d9e8bd04a8e6addda8d4caa87f2f7
baodvu@gmail.com <Inc6WJR1pd2h>
2019-11-10 23:26:41 -0800

    Add random plays

--- src/main/java/com/codesignal/csbot/listeners/handlers/music/PlayCommandHandler.java
+++ src/main/java/com/codesignal/csbot/listeners/handlers/music/PlayCommandHandler.java
@@ -22,15 +22,14 @@ import net.sourceforge.argparse4j.inf.Namespace;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PlayCommandHandler extends AbstractCommandHandler {
     private static final String GOOGLE_API_KEY = System.getenv("GOOGLE_API_KEY");
     private static final Map<String, String> RADIO_LINKS = Map.of(
             "npr", "https://npr-ice.streamguys1.com/live.mp3"
     );
-    private static final List<String> names = List.of("play", "skip", "stop", "pause", "seek");
+    private static final List<String> names = List.of("play", "skip", "stop", "pause", "seek", "playrandom");
 
     public List<String> getNames() { return names; }
     public String getShortDescription() { return "Tell the bot to say something."; }
@@ -46,12 +45,12 @@ public class PlayCommandHandler extends AbstractCommandHandler {
     }
 
     public void onMessageReceived(MessageReceivedEvent event, BotCommand botCommand) {
-        String song = botCommand.getCommandParams();
-        if (song.matches("^<.*://.*>$")) {
-            song = song.substring(1, song.length() - 1);
+        String query = botCommand.getCommandParams();
+        if (query.matches("^<.*://.*>$")) {
+            query = query.substring(1, query.length() - 1);
         }
-        if (RADIO_LINKS.containsKey(song)) {
-            song = RADIO_LINKS.get(song);
+        if (RADIO_LINKS.containsKey(query)) {
+            query = RADIO_LINKS.get(query);
         }
         MessageChannel channel = event.getChannel();
 
@@ -84,7 +83,6 @@ public class PlayCommandHandler extends AbstractCommandHandler {
                 channel.sendMessage("Track is not seekable").queue();
                 return;
             }
-            String query = botCommand.getCommandParams();
             if (query.startsWith("-")) {
                 long delta = parseDuration(query.substring(1));
                 track.setPosition(Math.max(0, track.getPosition() - delta));
@@ -109,61 +107,78 @@ public class PlayCommandHandler extends AbstractCommandHandler {
             manager.openAudioConnection(voiceChannel);
         }
 
-        JSONObject file = null;
-        final String trackUrl;
-        if (song.startsWith("http")) {
-            trackUrl = song;
+
+        List<JSONObject> files = new ArrayList<>();
+        final List<String> trackUrls = new ArrayList<>();
+
+        if (query.startsWith("http")) {
+            trackUrls.add(query);
         } else {
             try {
-                file = querySong(song);
+                if (botCommand.getCommandName().equals("playrandom")) {
+                    int splitIdx = query.indexOf(" ");
+                    if (splitIdx == -1) {
+                        channel.sendMessage("Syntax: playrandom <number of songs> <folder search terms>").queue();
+                        return;
+                    }
+                    files.addAll(queryRandomSongs(
+                            query.substring(splitIdx + 1),
+                            Math.min(10, Math.max(0, Integer.parseInt(query.substring(0, splitIdx))))));
+                } else {
+                    files.add(querySong(query));
+                }
             } catch (Exception e) {
                 event.getChannel().sendMessage("Error: " + e.getMessage()).queue();
                 return;
             }
-            if (file != null) {
-                trackUrl = String.format(
+            for (JSONObject file: files) {
+                trackUrls.add(String.format(
                         "https://www.googleapis.com/drive/v3/files/%s?alt=media&key=%s",
-                        file.getString("id"), GOOGLE_API_KEY);
+                        file.getString("id"), GOOGLE_API_KEY));
             }
-            else {
+            if (files.isEmpty()) {
                 event.getChannel().sendMessage("No songs found").queue();
                 return;
             }
         }
-        final String songTitle = file == null ? null : file.getString("name");
 
-        playerManager.loadItemOrdered(guild.getId(), trackUrl, new AudioLoadResultHandler() {
-            @Override
-            public void trackLoaded(AudioTrack track) {
-                String title = songTitle != null ? songTitle : track.getInfo().title;
-                channel.sendMessage("Adding to queue " + title).queue();
-                scheduler.queue(track, () -> {
-                    channel.sendMessage("Playing " + title).queue();
-                    return null;
-                });
-            }
+        int i = 0;
+        for (String trackUrl: trackUrls) {
+            final String songTitle = i < files.size() ? files.get(i++).getString("name") : null;
 
-            @Override
-            public void playlistLoaded(AudioPlaylist playlist) {
-                channel.sendMessage("Found " + playlist.getTracks().size() + " tracks").queue();
-                for (AudioTrack track: playlist.getTracks()) {
+            playerManager.loadItemOrdered(guild.getId(), trackUrl, new AudioLoadResultHandler() {
+                @Override
+                public void trackLoaded(AudioTrack track) {
+                    String title = songTitle != null ? songTitle : track.getInfo().title;
+                    channel.sendMessage("Adding to queue " + title).queue();
                     scheduler.queue(track, () -> {
-                        channel.sendMessage("Playing " + track.getInfo().title).queue();
+                        channel.sendMessage("Playing " + title).queue();
                         return null;
                     });
                 }
-            }
 
-            @Override
-            public void noMatches() {
-                channel.sendMessage("Nothing found by " + trackUrl).queue();
-            }
+                @Override
+                public void playlistLoaded(AudioPlaylist playlist) {
+                    channel.sendMessage("Found " + playlist.getTracks().size() + " tracks").queue();
+                    for (AudioTrack track : playlist.getTracks()) {
+                        scheduler.queue(track, () -> {
+                            channel.sendMessage("Playing " + track.getInfo().title).queue();
+                            return null;
+                        });
+                    }
+                }
 
-            @Override
-            public void loadFailed(FriendlyException exception) {
-                channel.sendMessage("Could not play: " + exception.getMessage()).queue();
-            }
-        });
+                @Override
+                public void noMatches() {
+                    channel.sendMessage("Nothing found by " + trackUrl).queue();
+                }
+
+                @Override
+                public void loadFailed(FriendlyException exception) {
+                    channel.sendMessage("Could not play: " + exception.getMessage()).queue();
+                }
+            });
+        }
     }
 
     private JSONObject querySong(String title) throws UnirestException {
@@ -178,6 +193,25 @@ public class PlayCommandHandler extends AbstractCommandHandler {
                 .asJson().getBody();
         JSONArray files = resp.getObject().getJSONArray("files");
         return files.isEmpty() ? null : files.getJSONObject(0);
+    }
+
+    private List<JSONObject> queryRandomSongs(String folderTitle, int count) throws UnirestException {
+        JsonNode resp = Unirest
+                .get(
+                "https://script.google.com/macros/s/AKfycbz5UIZG80cPz0naFrYJNMqIeRAKgs7dfJ5hlfOI2o-qENuO6nc/exec")
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, " +
+                        "like Gecko) Chrome/75.0.3770.100 Safari/537.36")
+                .queryString("title", folderTitle)
+                .asJson().getBody();
+        JSONArray files = resp.getObject().getJSONArray("files");
+        List<JSONObject> available = new ArrayList<>();
+        for (int i = 0; i < files.length(); ++i ) {
+            available.add(files.getJSONObject(i));
+        }
+        Collections.shuffle(available);
+        return available.subList(0, count);
     }
 
     private String formatDuration(long milliseconds) {
