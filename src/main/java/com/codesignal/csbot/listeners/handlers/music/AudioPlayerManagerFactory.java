cc2c5ca9d36940e579330ab1afcb3913010a46b4
baodvu@gmail.com <Inc6WJR1pd2h>
2019-11-10 20:43:28 -0800

    Extend socket timeout

--- src/main/java/com/codesignal/csbot/listeners/handlers/music/AudioPlayerManagerFactory.java
+++ src/main/java/com/codesignal/csbot/listeners/handlers/music/AudioPlayerManagerFactory.java
@@ -3,6 +3,9 @@ package com.codesignal.csbot.listeners.handlers.music;
 import com.sedmelluq.discord.lavaplayer.player.AudioConfiguration;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 
 public class AudioPlayerManagerFactory {
     private static DefaultAudioPlayerManager manager;
@@ -12,6 +15,14 @@ public class AudioPlayerManagerFactory {
              manager = new DefaultAudioPlayerManager();
              manager.getConfiguration().setResamplingQuality(AudioConfiguration.ResamplingQuality.HIGH);
              manager.getConfiguration().setOpusEncodingQuality(10);
+             RequestConfig requestConfig = RequestConfig.custom()
+                     .setConnectionRequestTimeout(2000)
+                     .setConnectTimeout(2000)
+                     .setSocketTimeout(10000)
+                     .build();
+             manager.setHttpBuilderConfigurator((HttpClientBuilder builder) -> {
+                 builder.setDefaultRequestConfig(requestConfig);
+             });
              AudioSourceManagers.registerRemoteSources(manager);
          }
          return manager;
